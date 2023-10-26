78f4b964233a4af983e1e1a070fdb10ab1e70b03
anti-duhring <mateus.limavn@gmail.com>
2023-10-26 16:12:42 -0300

    setup

---
+++ cmd/init.go
@@ -0,0 +1,69 @@
+/*
+Copyright © 2023 NAME HERE <EMAIL ADDRESS>
+*/
+package cmd
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/spf13/cobra"
+)
+
+// initCmd represents the init command
+var initCmd = &cobra.Command{
+	Use:   "init",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("init called")
+
+		if err := createInitAndObjectFolders(); err != nil {
+			panic(err)
+		}
+
+		if err := createRefFile(); err != nil {
+			panic(err)
+		}
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(initCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// initCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// initCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+func createInitAndObjectFolders() error {
+	return os.MkdirAll(".init/objects", os.ModePerm)
+}
+
+func createRefFile() error {
+	path := "./.init/ref.txt"
+
+	file, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	defer file.Close()
+
+	_, err = file.WriteString("0")
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
---
+++ main.go
@@ -0,0 +1,11 @@
+/*
+Copyright © 2023 anti-duhring
+
+*/
+package main
+
+import "github.com/anti-duhring/fit/cmd"
+
+func main() {
+	cmd.Execute()
+}
